f2b450b71c687942b2b7bff1d5d1331b72ac2b82
Mihail_Kozhevnikov <makozhevnikov@vtb.ru>
2019-08-12 00:46:58 +0400

    default

--- client/src/components/molecules/user-box-sender/index.jsx
+++ client/src/components/molecules/user-box-sender/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { FormattedMessage, injectIntl } from "react-intl";
 import "./user-box-sender.css";
 
-export const UserBoxSender = ({ username, handleReset }) => {
+export const UserBoxSender = ({ username, handleReset, intl }) => {
 	return (
 		<div className="complete-user-box">
 			<p className="complete-user-box__text">сообщение пользователю {username} успешно отправлено</p>
 			<button className="complete-user-box__button" onClick={handleReset}>
-				new mail
+				{/* new mail */}
+				<FormattedMessage id="test" defaultMessage="default message" />
 			</button>
 		</div>
 	);
 };
+
+// export const UserBoxSender = injectIntl(UserBoxSenderComponent);
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,14 @@
+import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createAppStore } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import { intlProvider } from "react-intl";
+import { addLocaleData } from "react-intl";
+import localedMessages from "./services/localization/locales";
 import { App } from "./modules";
+import { IntlProvider, FormattedMessage } from "react-intl";
 import "./styles/main.css";
 import "./styles/shared.css";
 import "./styles/keyframes.css";
@@ -11,14 +16,68 @@ import "./assets/fonts/index.css";
 
 import { UserBoxSender } from "./components";
 
+// import enLocale from "react-intl/locale-data/en";
+// import ruLocale from "react-intl/locale-data/ru";
+
+const locale = "en";
+
+// addLocaleData(enLocale);
+// addLocaleData(ruLocale);
+
+const messages = {
+	simple: "Hello world",
+	placeholder: "Hello {name}",
+	date: "Hello {ts, date}",
+	time: "Hello {ts, time}",
+	number: "Hello {num, number}",
+	plural: "I have {num, plural, one {# dog} other {# dogs}}",
+	select: "I am a {gender, select, male {boy} female {girl}}",
+	selectordinal: `I am the {order, selectordinal, 
+		  one {#st person} 
+		  two {#nd person}
+		  =3 {#rd person} 
+		  other {#th person}
+	  }`,
+	richtext: "I have <bold>{num, plural, one {# dog} other {# dogs}}</bold>",
+};
+
 const ROOT_ELEMENT = document.getElementById("root");
 const store = createAppStore();
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			{/* <App /> */}
-			<UserBoxSender />
+			<IntlProvider locale="en" messages={messages}>
+				<p>
+					<FormattedMessage id="simple" />
+					<br />
+					<FormattedMessage id="placeholder" values={{ name: "John" }} />
+					<br />
+					<FormattedMessage id="date" values={{ ts: Date.now() }} />
+					<br />
+					<FormattedMessage id="time" values={{ ts: Date.now() }} />
+					<br />
+					<FormattedMessage id="number" values={{ num: Math.random() * 1000 }} />
+					<br />
+					<FormattedMessage id="plural" values={{ num: 1 }} />
+					<br />
+					<FormattedMessage id="plural" values={{ num: 99 }} />
+					<br />
+					<FormattedMessage id="select" values={{ gender: "male" }} />
+					<br />
+					<FormattedMessage id="select" values={{ gender: "female" }} />
+					<br />
+					<FormattedMessage id="selectordinal" values={{ order: 1 }} />
+					<br />
+					<FormattedMessage id="selectordinal" values={{ order: 2 }} />
+					<br />
+					<FormattedMessage id="selectordinal" values={{ order: 3 }} />
+					<br />
+					<FormattedMessage id="selectordinal" values={{ order: 4 }} />
+					<br />
+					<FormattedMessage id="richtext" values={{ num: 99, bold: (...chunks) => <strong>{chunks}</strong> }} />
+				</p>
+			</IntlProvider>
 		</BrowserRouter>
 	</Provider>,
 	ROOT_ELEMENT
